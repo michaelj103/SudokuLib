c1b9161d25c1667698645044092120f406e7ec83
Michael Brandt <michaeljbrandt@icloud.com>
2020-04-25 15:49:54 -0700

    initial commit

---
+++ src/SudokuSolver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SudokuSolver
+//
+//  Created by Michael Brandt on 4/25/20.
+//  Copyright © 2020 Michael Brandt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
